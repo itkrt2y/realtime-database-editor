b992655b0476e3b27ba726b539a1af2188cb2e71
Tatsuya Itakura <itkrt2y.591721200@gmail.com>
2019-09-08 19:38:43 +0900

    delete settings

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,13 +1,13 @@
 import * as firebase from "firebase/app";
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCI_zUKRlQgdsDPpc8BxPVXtJQlmxcINUY",
-  authDomain: "editor-sample-c4570.firebaseapp.com",
-  databaseURL: "https://editor-sample-c4570.firebaseio.com",
-  projectId: "editor-sample-c4570",
+  apiKey: "<api key>",
+  authDomain: "<auth domain>",
+  databaseURL: "<database URL>",
+  projectId: "<project id>",
   storageBucket: "",
-  messagingSenderId: "568111051412",
-  appId: "1:568111051412:web:636bd3fed7aa99c4deba81"
+  messagingSenderId: "<message sender id>",
+  appId: "<app id>"
 });
 
 import "firebase/database";
